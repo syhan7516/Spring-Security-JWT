10dc2c8373c18cf069a4de79947731931d6481eb
Shin <syhan7516@naver.com>
2024-01-23 01:07:14 +0900

    회원가입 Controller 생성

---
+++ src/main/java/com/study/securityjwt/controller/JoinController.java
@@ -0,0 +1,26 @@
+package com.study.securityjwt.controller;
+
+import com.study.securityjwt.dto.JoinDto;
+import com.study.securityjwt.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class JoinController {
+
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDto joinDto) {
+        joinService.joinProcess(joinDto);
+
+        return "ok";
+    }
+}
