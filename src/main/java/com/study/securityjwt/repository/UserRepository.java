4bcbdb1a2a4ec79c0ad7e75452d40e0024acf9f0
Shin <syhan7516@naver.com>
2024-01-23 01:04:07 +0900

    유저 존재 여부 확인 메서드 추가

--- src/main/java/com/study/securityjwt/repository/UserRepository.java
+++ src/main/java/com/study/securityjwt/repository/UserRepository.java
@@ -4,5 +4,7 @@ import com.study.securityjwt.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-
+    
+    // 유저 존재 여부
+    Boolean existsByUsername(String username);
 }
