c501d2c3ecb8c06edffaef5e24ddcab20b12e8e0
Shin <syhan7516@naver.com>
2024-01-23 11:54:29 +0900

    회원 조회 메서드 추가

--- src/main/java/com/study/securityjwt/repository/UserRepository.java
+++ src/main/java/com/study/securityjwt/repository/UserRepository.java
@@ -7,4 +7,7 @@ public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     
     // 유저 존재 여부
     Boolean existsByUsername(String username);
+
+    // 회원 조회
+    UserEntity findByUsername(String username);
 }
